596031c99a8171d1b1534d523469ae9553a2a5e0
Burdusa Daniel <danielburdusa26@gmail.com>
2023-09-07 12:51:22 +0200

    milestone 3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,24 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        String query = "select countries.country_id as id_paese , countries.name as nome, regions.name as regione , continents.name as continente\n" +
-                "from countries \n" +
-                "join regions  on countries.region_id  = regions.region_id \n" +
-                "join continents on regions.continent_id  = continents.continent_id \n" +
-                "order by countries.name ;";
+        Scanner input = new Scanner(System.in);
+
+        String query = "select countries.country_id as id_paese , countries.name as nome, regions.name as regione , continents.name as continente \n" +
+                "from countries\n" +
+                "join regions  on countries.region_id  = regions.region_id\n" +
+                "join continents on regions.continent_id  = continents.continent_id\n" +
+                "where countries.name like ?\n" +
+                "order by countries.name;";
         String url = "jdbc:mysql://localhost:3306/nazioni";
         String user = "root";
         String password = "root";
         try (Connection con = DriverManager.getConnection(url, user, password)) {
+            System.out.println("cerca una nazione");
+            String ricerca = input.nextLine();
             try (PreparedStatement ps = con.prepareStatement(query)) {
+                ps.setString(1,"%"+ricerca+"%");
                 try (ResultSet rs = ps.executeQuery()) {
                     while (rs.next()) {
                         String idNazione = rs.getString("id_paese");
